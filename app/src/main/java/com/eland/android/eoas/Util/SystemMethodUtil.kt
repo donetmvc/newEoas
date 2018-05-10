2dfa560933ef94e686c9ef9a9bc49a2b61f41504
liuwenbin <liu.wenbin@eland.co.kr>
2018-05-10 13:39:43 +0800

    Some thing

--- app/src/main/java/com/eland/android/eoas/Util/SystemMethodUtil.kt
+++ app/src/main/java/com/eland/android/eoas/Util/SystemMethodUtil.kt
@@ -2,16 +2,13 @@ package com.eland.android.eoas.Util
 
 import android.content.Context
 
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
 import java.util.ArrayList
 import android.app.ActivityManager
 import android.app.ActivityManager.RunningServiceInfo
 
 
 import com.eland.android.eoas.Model.LoginInfo
+import java.io.*
 
 /**
  * Created by liu.wenbin on 15/12/9.
